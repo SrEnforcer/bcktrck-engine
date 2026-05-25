2e268bdfb23c39eeb059e79e31b7dfde64789255
preTHOR <admin@prethor.nl>
2026-05-25 18:41:37 +0200

    feat(icons): add cannabis icon support in default registry
    Add a built-in cannabis icon node and wire it into icon lookup helpers so style-driven icon rendering can use it without custom registry extensions.

--- src/icons/registry.ts
+++ src/icons/registry.ts
@@ -44,6 +44,16 @@ import { intoMap, intoSet } from '@tsfpp/prelude'
  */
 export type IconNode = LucideIconNode
 
+const cannabisPath =
+  'M7 12c-1.5 0-4.5 1.5-5 3 3.5 1.5 6 1 6 1-1.5 1.5-2 3.5-2 5 ' +
+  '2.5 0 4.5-1.5 6-3 1.5 1.5 3.5 3 6 3 0-1.5-.5-3.5-2-5 0 0 2.5.5 6-1-.5-1.5-3.5-3-5-3 ' +
+  '1.5-1 4-4 4-6-2.5 0-5.5 1.5-7 3 0-2.5-.5-5-2-7-1.5 2-2 4.5-2 7-1.5-1.5-4.5-3-7-3 0 2 2.5 5 4 6'
+
+const Cannabis: IconNode = [
+  ['path', { d: 'M12 22v-4' }],
+  ['path', { d: cannabisPath }],
+]
+
 /**
  * Supported icon anchor positions within node bounds.
  */
@@ -202,6 +212,7 @@ const ICON_REGISTRY = mapFromEntries<string, IconNode>([
   ['shapes', Shapes],
   ['wine', Wine],
   ['beer', Beer],
+  ['cannabis', Cannabis],
   ['bottle-wine', BottleWine],
   ['gem', Gem],
   // Identity / Credential
